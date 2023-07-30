2be4a6b5dfa275c0293ee3f82125899682c0dfe1
mrahmatt74 <muhamaadrahmatt@gmail.com>
2023-07-30 09:26:59 +0700

    menghapus defer

--- connectDB/connectDB.go
+++ connectDB/connectDB.go
@@ -9,16 +9,14 @@ import (
 
 func GetConnPostgres(host, port, user, password, dbname, driver string) (*sql.DB, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
 	db, err := sql.Open(driver, psqlInfo)
 
 	if err != nil {
 		return nil, err
 	}
-	defer db.Close()
+	// defer db.Close()
 
 	err = db.Ping()
-
 	if err != nil {
 		return nil, err
 	}
--- main.go
+++ main.go
@@ -11,87 +11,81 @@ import (
 )
 
 func main() {
-	if err := godotenv.Load(".env"); err != nil {
-		log.Error().Msg("Error loading .env file")
+	// Untuk memuat file .env sebagai konfigurasi
+	err := godotenv.Load("config/.env")
+
+	if err != nil {
+		log.Error().Msg(err.Error())
 		os.Exit(1)
 	}
-
 	DB_HOST := os.Getenv("DB_HOST")
 	DB_NAME := os.Getenv("DB_NAME")
 	DB_PORT := os.Getenv("DB_PORT")
 	DB_USER := os.Getenv("DB_USER")
-	DB_PASSWORD := os.Getenv("POSTGRES_PASSWORD")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
 	DB_DRIVER := os.Getenv("DB_DRIVER")
 	PORT := os.Getenv("PORT")
 
-	log.Info().Msg("DB_HOST: " + DB_HOST)
-	log.Info().Msg("DB_USER:" + DB_USER)
-	log.Info().Msg("DB_PASSWORDD:" + DB_PASSWORD)
-	log.Info().Msg("DB_NAME:" + DB_NAME)
-	log.Info().Msg("PORT:" + DB_PORT)
-	log.Info().Msg("DB_DRIVER:" + DB_DRIVER)
-	log.Info().Msg("DB_DRIVER:" + PORT)
+	log.Info().Msg(DB_HOST)
+	log.Info().Msg(DB_NAME)
+	log.Info().Msg(DB_PORT)
+	log.Info().Msg(DB_USER)
+	log.Info().Msg(DB_PASSWORD)
+	log.Info().Msg(DB_DRIVER)
+	log.Info().Msg(PORT)
 
 	db, errConn := connectDB.GetConnPostgres(DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, DB_DRIVER)
-
 	if errConn != nil {
 		log.Error().Msg(errConn.Error())
 		os.Exit(1)
 	}
+	fmt.Println("Successfully connected!")
 
-	fmt.Println("Successfully connected")
-	// DB STRUCT INITIALIZE
-
+	// DB struct initialize
 	DB := query.DB{Conn: db}
 
-	err := DB.Create(&query.Customers{
-		Name:  "Tiar",
+	// Create customer
+	err = DB.Create(&query.Customers{
+		Name:  "Muhammad Rahmat",
 		Phone: "085770854766",
-		Email: "muhamaadrahmatt@gmail.com",
+		Email: "rahmat@mail.com",
 		Age:   21,
 	})
-
+	fmt.Println("err", err)
 	if err != nil {
 		log.Error().Msg(errConn.Error())
 		os.Exit(1)
 	}
-
 	fmt.Println("Insert Data Berhasil")
 
-	// read
+	// Read customer
 	result, err := DB.Read()
-
 	if err != nil {
 		log.Error().Msg(errConn.Error())
 		os.Exit(1)
 	}
-
 	fmt.Println(result)
 
-	// update
+	// Update customer
 	err = DB.Update(&query.Customers{
 		Id:    1,
-		Name:  "Bagas",
-		Phone: "085781544293",
-		Email: "bagas@gmail.com",
-		Age:   23,
+		Name:  "Budi",
+		Phone: "012345564",
+		Email: "budi@example.com",
+		Age:   31,
 	})
-
 	if err != nil {
 		log.Error().Msg(errConn.Error())
 		os.Exit(1)
 	}
-
 	fmt.Println("Update Berhasil")
 
-	// delete
-	err = DB.Delete(2)
-
+	// Delete customer
+	err = DB.Delete(1)
 	if err != nil {
 		log.Error().Msg(errConn.Error())
 		os.Exit(1)
 	}
-
-	fmt.Println("Update Berhasil")
+	fmt.Println("Delete Berhasil")
 
 }
